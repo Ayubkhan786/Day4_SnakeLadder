e9dd90de0c0a0e7ead496cfc6f2bd6fcfdab2ae9
your Name <mohamedabdulayubm@gmail.com>
2022-06-17 22:55:24 +0530

    Options

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -1,5 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to snake Ladder Game!");
 int p1sp = 0;
-int p2sp = 0;
+//int p2sp = 0;
+Random random = new Random();
+int diecount=random.Next(1,7);
+
+
+
+
+
+
 Random rand = new();
+int dieroll1 = 0, dieroll2 = 0;
+while (dieroll1 < 100 && dieroll2 < 100)
+    switch (rand.Next(0, 3))
+    {
+        case 0:
+            Console.WriteLine("No Play");
+
+
+            break;
+        case 1:
+            Console.WriteLine("Ladder");
+            //Console.WriteLine(dieroll1 +=1);
+            Console.WriteLine(p1sp += diecount ); 
+            dieroll1++;
+
+            break;
+        case 2:
+            Console.WriteLine("Snake");
+            //Console.WriteLine(dieroll2 += 1);
+            Console.WriteLine(p1sp -= diecount);
+            dieroll2++;
+            break;
+
+    }
+
